f2150f1030f154e2e594eb8f287be5843e2964e9
Chronophylos <nikolai@chronophylos.com>
2020-12-04 11:19:28 +0100

    get streams from the top 100 games

--- src/bin/get-streams.rs
+++ src/bin/get-streams.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Result};
 use async_compat::Compat;
+use futures::future::try_join_all;
 use reqwest::{
     header::{HeaderMap, HeaderName, HeaderValue, ACCEPT},
     Client, StatusCode,
@@ -8,7 +9,8 @@ use serde::Deserialize;
 use std::borrow::Cow;
 use twitch_gift_farm::Config;
 
-const KRAKEN_STREAMS: &str = "https://api.twitch.tv/kraken/streams/";
+const KRAKEN_STREAMS: &str = "https://api.twitch.tv/kraken/streams";
+const KRAKEN_TOP_GAMES: &str = "https://api.twitch.tv/kraken/games/top";
 const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 const CLIENT_ID: &str = "34afn666979w6kmmr6b1bcnagfv6s3";
 
@@ -28,17 +30,64 @@ struct Channel<'a> {
 }
 
 #[derive(Debug, Deserialize)]
+struct TopGamesResponse<'a> {
+    top: Vec<Game<'a>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Game<'a> {
+    game: GameData<'a>,
+}
+
+#[derive(Debug, Deserialize)]
+struct GameData<'a> {
+    name: Cow<'a, str>,
+}
+
+#[derive(Debug, Deserialize)]
 struct ErrorResponse<'a> {
     error: Cow<'a, str>,
     status: u16,
     message: Cow<'a, str>,
 }
 
-async fn get_streams_page<'a>(client: &Client, offset: u16) -> Result<Vec<Cow<'a, str>>> {
+async fn get_top_games<'a>(client: &Client, offset: u16) -> Result<Vec<Cow<'a, str>>> {
+    Compat::new(async {
+        let resp = client
+            .get(KRAKEN_TOP_GAMES)
+            .query(&[("offset", offset), ("limit", 100)])
+            .send()
+            .await?;
+
+        if resp.status() == StatusCode::BAD_REQUEST {
+            let error = resp.json::<ErrorResponse>().await?;
+            return Err(anyhow!("Could not get top games: {}", error.message));
+        }
+
+        let games = resp
+            .error_for_status()?
+            .json::<TopGamesResponse>()
+            .await?
+            .top
+            .into_iter()
+            .map(|game| game.game.name)
+            .collect();
+
+        Ok(games)
+    })
+    .await
+}
+
+async fn get_streams_page<'a>(
+    client: &Client,
+    game: &str,
+    offset: u16,
+) -> Result<Vec<Cow<'a, str>>> {
     Compat::new(async {
         let resp = client
             .get(KRAKEN_STREAMS)
             .query(&[("offset", offset), ("limit", 100)])
+            .query(&[("game", game)])
             .send()
             .await?;
 
@@ -61,6 +110,25 @@ async fn get_streams_page<'a>(client: &Client, offset: u16) -> Result<Vec<Cow<'a
     .await
 }
 
+async fn get_all_streams_for_game<'a>(client: &Client, game: String) -> Result<Vec<Cow<'a, str>>> {
+    let mut futures = Vec::with_capacity(10);
+
+    for i in 0..=9 {
+        let offset = i * 100;
+        futures.push(get_streams_page(&client, &game, offset));
+    }
+
+    let streams = try_join_all(futures)
+        .await?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<Cow<'a, str>>>();
+
+    println!("Found {} channels streaming {}", streams.len(), game);
+
+    Ok(streams)
+}
+
 async fn get_streams<'a>() -> Result<Vec<Cow<'a, str>>> {
     let mut headers = HeaderMap::new();
     headers.insert(
@@ -77,22 +145,29 @@ async fn get_streams<'a>() -> Result<Vec<Cow<'a, str>>> {
         .user_agent(APP_USER_AGENT)
         .build()?;
 
-    let mut streams = Vec::with_capacity(1000);
+    let games = get_top_games(&client, 0).await?;
 
-    for i in 0..=9 {
-        let offset = i * 100;
-        streams.append(&mut get_streams_page(&client, offset).await?);
+    println!("Found {} games", games.len());
+
+    let max_streams = 1000 * games.len();
+
+    println!("Getting up to {} streams", max_streams);
+
+    let mut futures = Vec::with_capacity(games.len());
+
+    for game in games {
+        futures.push(get_all_streams_for_game(&client, game.to_string()));
     }
+
+    let streams = try_join_all(futures).await?.into_iter().flatten().collect();
 
     Ok(streams)
 }
 
 fn main() -> Result<()> {
-    println!("Getting 1000 live channels");
-
     let mut channels = smol::block_on(get_streams())?;
 
-    println!("Found {} channels", channels.len());
+    println!("Found {} channels currently streaming", channels.len());
 
     let mut config = Config::load()?;
     let old_count = config.channels.len();
